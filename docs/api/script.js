e8b8115305b117b241b9c8ff0247a8bda6ca770b
elastestci <elastestci@gmail.com>
2017-05-17 14:38:27 +0000

    Deploy from Travis CI

---
+++ docs/api/script.js
@@ -0,0 +1,17 @@
+function loadSwagger(urls) {
+  // Build a system
+  const ui = SwaggerUIBundle({
+    url: urls,
+    dom_id: '#swagger-ui',
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  })
+
+  window.ui = ui
+}
